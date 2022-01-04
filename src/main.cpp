2c8b170dc4df63af35a24cebe6ceec3bf7c1f1fb
smhex <{ID}+{username}@users.noreply.github.com>
2022-01-04 21:26:36 +0100

    Added support for MKR ENV shield

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,60 @@
 #include <Arduino.h>
-#include <MKRENV.h>
+#include <Arduino_MKRENV.h>
 
+// setup the board an all variables
 void setup() {
-  // put your setup code here, to run once:
+  
+  // Init serial line with 9600 baud
+  Serial.begin(9600);
+  while (!Serial);
+
+  // check if MKR ENV shield is install/present
+  if (!ENV.begin()) {
+    Serial.println("Failed to initialize MKR ENV shield!");
+    while (1);
+  }
 }
 
+// main loop - reads/writes commands and sensor values
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read all the sensor values
+  float temperature = ENV.readTemperature();
+  float humidity    = ENV.readHumidity();
+  float pressure    = ENV.readPressure();
+  float illuminance = ENV.readIlluminance();
+  float uva         = ENV.readUVA();
+  float uvb         = ENV.readUVB();
+  float uvIndex     = ENV.readUVIndex();     
+
+  // print each of the sensor values
+  Serial.print("Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humidity    = ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.print("Pressure    = ");
+  Serial.print(pressure);
+  Serial.println(" kPa");
+
+  Serial.print("Illuminance = ");
+  Serial.print(illuminance);
+  Serial.println(" lx");
+
+  Serial.print("UVA         = ");
+  Serial.println(uva);
+
+  Serial.print("UVB         = ");
+  Serial.println(uvb);
+
+  Serial.print("UV Index    = ");
+  Serial.println(uvIndex);
+
+  // print an empty line
+  Serial.println();
+
+  // wait 1 second to print again
+  delay(1000);
 }
